da1f0f4fabf5a7cae3117132f980bf9ce555bfe0
Daniel <daniel.mwangi@student.moringaschool.com>
2023-12-12 12:38:39 +0300

    committed schema with sqlalchemy

--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -6,6 +6,10 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 class Student(Base):
+    __tablename__ = 'Student'
+
+    id = Column (Integer(), Primary_key = TRUE)
+    name = Column (String())
     pass
 
 if __name__ == '__main__':
